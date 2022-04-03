525efb2b8878af666ef9fa0d5ff3bbe27fe1e9a3
Cypgain <cypgain@gmail.com>
2022-04-03 19:50:38 +0200

    ref: change naming for file from camelCase by kebab-case

--- src/common/decorators/index.ts
+++ src/common/decorators/index.ts
@@ -1,4 +1,4 @@
-export * from './getCurrentUser.decorator';
-export * from './getCurrentUserId.decorator';
+export * from './get-current-user.decorator';
+export * from './get-current-user-id.decorator';
 export * from './public.decorator';
 export * from './roles.decorator';
