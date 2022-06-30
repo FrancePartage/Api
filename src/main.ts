5d132431256b6b180b514ae9a2ac816087c9a9d6
Tom Brulin <tom.brulin@viacesi.fr>
2022-06-30 08:58:36 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -15,18 +15,6 @@ async function bootstrap() {
 	app.use(cors(corsOptions));
 	*/
 
-	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*');
-		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
-		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-		next();
-	});
-
-	app.enableCors({
-		allowedHeaders:"*",
-		origin: "*"
-	});
-
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
