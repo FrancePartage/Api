e3572bf1f88798bb94c6e8b9dd40b282983b9508
Tom Brulin <tom.brulin@viacesi.fr>
2022-06-30 08:52:07 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,25 @@ const cors = require('cors');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+	/*
 	const corsOptions ={
 			origin: 'http://francepartage.zapto.org', 
 			credentials: true,            
 			optionSuccessStatus: 200
 	}
 	app.use(cors(corsOptions));
+	*/
 
 	app.use((req, res, next) => {
 		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
 		next();
+	});
+
+	app.enableCors({
+		allowedHeaders:"*",
+		origin: "*"
 	});
 
   app.useGlobalPipes(new ValidationPipe());
